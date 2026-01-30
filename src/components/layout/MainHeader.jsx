0923d089144b852152f5386278d6abf97860fcfb
kasarshravani04 <shravanikasar5@gmail.com>
2026-01-30 16:08:49 +0530

    added nav section

--- src/components/layout/MainHeader.jsx
+++ src/components/layout/MainHeader.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { NavLink,Link } from "react-router-dom";
+import logoM from '../../assets/logo/O.B..png';
+import Logo from '../../assets/logo/Logo.1.png';
 
 const MainHeader = () => {
   return (
-    <div>MainHeader</div>
+      <header className="flex justify-between items-center border-b border-black py-6 layout">
+         <div>
+            <Link to="/" className="sm:hidden">
+                <img src={logoM} alt="our_Blooms" width={100} height={30} loading="eager" fetchPriority="high"
+                     className="w-full h-6 object-contain"/>  
+            </Link>
+            <Link to="/" className="hidden sm:block">
+                <img src={Logo} alt="Our_Blooms" width={100} height={20} loading="eager" fetchPriority="high"
+                     className="w-full h-7 object-contain hidden sm:block"/>
+            </Link>
+         </div>
+         <div>
+           <nav>
+              <ul className="flex gap-6">
+                <li>
+                   <NavLink to="/gallery" className={
+                      ({ isActive }) => isActive? "text-sm font-bold underline underline-offset-4" : "font-bold text-sm"
+                   }>
+                     Gallery
+                   </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/about" className={
+                      ({ isActive }) => isActive? "text-sm font-bold underline underline-offset-4" : "font-bold text-sm"
+                   }>
+                     About
+                   </NavLink>
+                </li>
+                <li>   
+                   <NavLink to="/contact" className={
+                      ({ isActive }) => isActive? "text-sm font-bold underline underline-offset-4" : "font-bold text-sm"
+                   }>
+                     Contact
+                   </NavLink>
+                </li>
+              </ul>
+           </nav>
+         </div>
+      </header>
   )
 }
 
-export default MainHeader
+export default MainHeader;
