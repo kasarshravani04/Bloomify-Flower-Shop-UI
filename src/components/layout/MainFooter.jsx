695116c93c6d5cbf679a02da6f71941aba3dbcc4
kasarshravani04 <shravanikasar5@gmail.com>
2026-02-01 00:43:44 +0530

    added footer

--- src/components/layout/MainFooter.jsx
+++ src/components/layout/MainFooter.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo/O.B..png";
 const MainFooter = () => {
   return (
-    <div>MainFooter</div>
-  )
-}
+    <footer className="layout p-6 rounded-t-4xl footer_background min-h-[80vh] bg-no-repeat bg-cover bg-fixed flex flex-col justify-end">
+      <div className="text-white space-y-8">
+        <div>
+          <h2 className="text-5xl md:text-9xl font-extrabold text-center">
+            OB.
+          </h2>
+        </div>
+        <div className="flex justify-between items-center ">
+          <address>Miami, FL</address>
+          <Link to="#">Contact</Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default MainFooter
+export default MainFooter;
