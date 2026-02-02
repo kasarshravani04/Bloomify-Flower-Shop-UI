b1140abfa8c7009fc4fd1145bc40a3f6a8674bf6
kasarshravani04 <shravanikasar5@gmail.com>
2026-02-02 15:45:43 +0530

    import gallery images

---
+++ src/assets/gallery/index.jsx
@@ -0,0 +1,12 @@
+import gallery1 from "./gallery1";
+import gallery2 from "./gallery2";
+import gallery3 from "./gallery3";
+import gallery4 from "./gallery4";
+import gallery5 from "./gallery5";
+import gallery6 from "./gallery6";
+import gallery7 from "./gallery7";
+import gallery8 from "./gallery8";
+import gallery9 from "./gallery9";
+
+
+export {gallery1,gallery2,gallery3,gallery4,gallery5,gallery6,gallery7,gallery8,gallery9};
