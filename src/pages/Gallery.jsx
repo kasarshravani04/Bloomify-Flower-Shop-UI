d994ae0b17c874474255b57a9ef045f59b7e11da
kasarshravani04 <shravanikasar5@gmail.com>
2026-02-02 15:30:28 +0530

    creating gallery page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,6 +1,16 @@
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <section>
+      <header className="text-5xl md:text-8xl font-bold">
+         <h3>Gallery</h3>
+      </header>
+      <div className="md:grid grid-cols-12 gap-4 my-20">
+         <aside className="grid-cols-2 text-text font-bold text-2xl">
+          <h3>SEASONAL ARRANGEMENTS</h3>
+         </aside>
+         
+      </div>
+    </section>
   )
 }
 
