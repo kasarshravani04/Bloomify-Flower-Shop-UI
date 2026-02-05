f576a3a405a582784981a5f651533a0d343b404b
kasarshravani04 <shravanikasar5@gmail.com>
2026-02-05 23:58:43 +0530

    added contact page information

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,16 @@
 const Contact = () => {
   return (
     <section>
+        <header className=" text-2xl my-16 font-bold text-text">
+           <h2>CONTACT US</h2>
+        </header>
         <div>
-            
+            <h3 className="text-6xl font-bold text-green-700  text-center">Fresh Flower for Every Moment</h3>
+            <div className="px-6 py-2 text-lg text-black justify-center">
+              <button className="bg-green-700 rounded-2xl">Shop Now</button>
+              <button className="bg-white">View Connection</button>
+            </div>
+           
         </div>
     </section>
   )
