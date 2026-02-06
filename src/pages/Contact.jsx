0bac1b7e28b447075beb2267f85edf6a9cc04b91
kasarshravani04 <shravanikasar5@gmail.com>
2026-02-06 11:41:44 +0530

    fixing responsive design

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,14 +4,13 @@ const Contact = () => {
   return (
     <section>
         <header className=" text-2xl my-16 font-bold text-text">
-           <h2>CONTACT US</h2>
+           <h2>CONTACT</h2>
         </header>
         <div>
             <h3 className="text-6xl font-bold text-green-700  text-center">Fresh Flower for Every Moment</h3>
-            <div className="px-6 py-2 text-lg text-black justify-center">
-              <button className="bg-green-700 rounded-2xl">Shop Now</button>
-              <button className="bg-white">View Connection</button>
-            </div>
+            <button className="bg-green-700 rounded px-6 py-2 text-black cursor-pointer">Shop Now</button>
+            <button className="bg-white  rounded px-6 py-2 text-black cursor-pointer border-green-700">View Connection</button>
+            
            
         </div>
     </section>
